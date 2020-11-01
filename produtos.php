0309bad94cc461f06e96d43fdbb3c519f4b23379
Nayara <nayara.lopes2007@gmail.com>
2020-10-31 21:50:13 -0300

    Adicionando Dropdown em categorias

--- produtos.php
+++ produtos.php
@@ -1,85 +1,85 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "fseletro";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "fseletro";
 
-    //Criando conexão
-    $conn = mysqli_connect($servername, $username, $password, $database);
+  //Criando conexão
+  $conn = mysqli_connect($servername, $username, $password, $database);
 
-    // Verificando conexão
-    if(!$conn){
-        die("A conexão ao Banco falhou: " .mysqli_connect_error());
-    }
-
+  // Verificando conexão
+  if(!$conn){
+    die("A conexão ao Banco falhou: " .mysqli_connect_error());
+  }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/styles.css" />
-    <script src="./js/scripts.js"></script>
-    <title>Produtos - Full Stack Eletro</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="./js/scripts.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./css/styles.css" />
+  <title>Produtos - Full Stack Eletro</title>
 </head>
-
 <body>
-    <?php include_once('menu.php') ?>
-    <header>
-        <h1>Produtos</h1>
-    </header>
-    <hr/>
-    <!--Iniciando os Produtos-->
-    <main class="pagProdutos">
-        <section class="categorias">
-            <h3>Categorias</h3>
-            <ul>
-                <li onclick="exibirTodos('todos')">Todos(12)</li>
-                <li onclick="exibirCategoria('geladeira')">Geladeiras(3)</li>
-                <li onclick="exibirCategoria('fogao')">Fogões(2)</li>
-                <li onclick="exibirCategoria('microondas')">Microondas(3)</li>
-                <li onclick="exibirCategoria('lavadoraDeRoupas')">Lavadora de Roupas(2)</li>
-                <li onclick="exibirCategoria('lavaLoucas')">Lava Louças(2)</li>
-            </ul>
-        </section>
+  <!-- Menu -->
+  <?php 
+    include_once('menu.html') 
+  ?>
+  <header class="d-flex justify-content-center">
+  <h1>Produtos</h1>
+</header>
 
-        <section class="produtos">
-            <?php
-                $sql = "select * from produtos";
-                $result = $conn->query($sql);
+  <!--Iniciando os Produtos-->
+  <main class=" container-fluid">
+    <div class="dropdown d-flex justify-content-center p-5">
+        <a class="btn text-white dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu border-0 rounded" aria-labelledby="dropdownMenuLink">
+          <ul>
+            <li  class="dropdown-item" onclick="exibirTodos('todos')">Todos(12)</li>
+            <li  class="dropdown-item" onclick="exibirCategoria('geladeira')">Geladeiras(3)</li>
+            <li  class="dropdown-item" onclick="exibirCategoria('fogao')">Fogões(2)</li>
+            <li  class="dropdown-item" onclick="exibirCategoria('microondas')">Microondas(3)</li>
+            <li  class="dropdown-item" onclick="exibirCategoria('lavadoraDeRoupas')">Lavadora de Roupas(2)</li>
+            <li  class="dropdown-item" onclick="exibirCategoria('lavaLoucas')">Lava Louças(2)</li>
+          </ul>
+      </div>
+    </div>
+    <section class="row d-flex justify-content-center mx-1">
+      <?php
+        $sql = "select * from produtos";
+        $result = $conn->query($sql);
 
-                if($result->num_rows > 0){
-                    while($rows = $result->fetch_assoc()){
-                        
-            ?>
-             <div class="box_produto" style="display: block;" id="<?php echo $rows["categoria"]; ?>" >
-                <div class="box_img">
-                    <img src="<?php echo $rows["imagem"]; ?>" alt="Geladeira 540 Litros" width="150px" onclick="destaque(this)">
-                </div>
-                <br/>
-                <p class="descricao"><?php echo $rows["descricao"]; ?></p>
-                <hr/>
-                <p class="antigo">R$ <?php echo $rows["preco"]; ?></p>
-                <p class="preco">R$ <?php echo $rows["precofinal"]; ?></p>
+        if($result->num_rows > 0){
+          while($rows = $result->fetch_assoc()){
+      ?>
+            <div class="box_produto mx-auto" style="display: block;" id="<?php echo $rows["categoria"]; ?>" >
+              <div class="box_img">
+                <img src="<?php echo $rows["imagem"]; ?>" alt="Geladeira 540 Litros" width="150px" onclick="destaque(this)">
+              </div>
+              <br/>
+              <p class="descricao"><?php echo $rows["descricao"]; ?></p>
+              <hr/>
+              <p class="antigo">R$ <?php echo $rows["preco"]; ?></p>
+              <p class="preco">R$ <?php echo $rows["precofinal"]; ?></p>
             </div>
-            <?php
-                    }
-                }else{
-                    echo "Nenhum produto cadastrado!!!";
-                }
-            ?>            
+        <?php
+          }
+        }else{
+          echo "Nenhum produto cadastrado!!!";
+        }
+        ?>            
            
-        </section>
-    </main>
-    <br/><br/><br/>
-    <br/>
-    <footer id="rodape">
-        <p id="formas_pagamento">Formas de pagamento:</p>
-        <img src="./img/pagamentos-bandeiras-01.png" alt="Formas de Pagamento">
-        <p>&copy; Recode Pro 2020</p>
-    </footer>
+    </section>
+  </main>
+ 
+  <?php 
+    include('rodape.html');
+  ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
-
 </html>
