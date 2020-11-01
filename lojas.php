d3ed4bb5b3d3f83bcd4a7f183e36ff99a510c28f
Nayara <nayara.lopes2007@gmail.com>
2020-10-31 21:52:39 -0300

    Adicionando BootStrap class container

--- lojas.php
+++ lojas.php
@@ -1,49 +1,60 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "fseletro";
+
+  //Criando conexão
+  $conn = mysqli_connect($servername, $username, $password, $database);
+
+  // Verificando conexão
+  if(!$conn){
+    die("A conexão ao Banco falhou: " .mysqli_connect_error());
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/styles.css" />
-    <title>Nossas lojas - Full Stack Eletro</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/styles.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <title>Nossas lojas - Full Stack Eletro</title>
 </head>
-
 <body>
-    <?php include_once('menu.php') ?>
-    <header>
-        <h1>Nossas lojas</h1>
-    </header>
-    <hr/>
+  <?php include_once('menu.html') ?>
+  <header>
+      <h1 class="mt-2">Nossas lojas</h1>
+  </header>
+  <hr/>
+  <section class="container-fluid d-flex flex-column">
+    <?php
+      $sql = "select * from lojas";
+      $result = $conn->query($sql);
 
-    <section class="lojas">
-        <div class="box_lojas">
-            <h3>Rio de Janeiro</h3>
-            <p>Avenida Presidente Vargas, 5000</p>
-            <p>10 &ordm; andar</p>
-            <p>Centro</p>
-            <p>(21) 3333-3333</p>
-        </div>
-        <div class="box_lojas">
-            <h3>São Paulo</h3>
-            <p>Avenida Paulista, 985</p>
-            <p>3 &ordm; andar</p>
-            <p>Jardins</p>
-            <p>(11) 4444-4444</p>
-        </div>
-        <div class="box_lojas">
-            <h3>Santa Catarina</h3>
-            <p>Rua Major &Aacute;vila, 370</p>
-            <p>Vila Mariana</p>
-            <p>(47) 5555-5555</p>
-        </div>
-    </section>
-    <br/><br/>
-
-    <footer id="rodape">
-        <p id="formas_pagamento">Formas de pagamento:</p>
-        <img src="./img/pagamentos-bandeiras-01.png" alt="Formas de Pagamento" />
-        <p>&copy; Recode Pro 2020</p>
-    </footer>
+      if($result->num_rows > 0){
+        while($rows = $result->fetch_assoc()){
+    ?>
+          <div>
+            <h3><?php echo $rows["cidade"]; ?></h3>
+            <p><?php echo $rows["endereco"]; ?></p>
+            <p><?php echo "Número ", $rows["numero"]; ?></p>
+            <p><?php echo $rows["andar"],"º andar" ?></p>
+            <p><?php echo $rows["bairro"]; ?></p>
+            <p><?php echo $rows["telefone"]; ?></p>
+          </div>
+    <?php
+        }
+      }else{
+        echo "Nenhum produto cadastrado!!!";
+      }
+    ?>    
+  </section>
+  <?php
+    include('rodape.html');
+  ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
-
 </html>
