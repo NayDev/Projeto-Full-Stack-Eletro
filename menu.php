be83c66fe506f934006625b6f4f3ab48238ef57f
Nayara <nayara.lopes2007@gmail.com>
2020-10-31 21:43:39 -0300

    Adicionando nav no menu

--- menu.php
+++
@@ -1,8 +0,0 @@
-   <!-- Inicio Menu-->
-   <nav class="menu">
-        <a class="logo" href="./index.php"></a>
-        <a href="./produtos.php">Nossos Produtos</a>
-        <a href="./lojas.php">Nossas Lojas</a>
-        <a href="./fale_conosco.php">Fale Conosco</a>
-    </nav>
-    <!-- Final Menu-->
